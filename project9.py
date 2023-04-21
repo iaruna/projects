7011649ed298b903ab770e0ca12de59f4d87e4a3
iaruna <arunakumariofficial@gmail.com>
2023-04-21 23:18:00 +0530

    Print Colored Text

---
+++ project9.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLACK+Back.GREEN+"Hi It's Aruna Kumari "+ Fore.GREEN+ Back.BLACK+"I am Python Developer.")
+print(Back.CYAN+"Hi It's Aruna Kumari")
+print(Fore.RED + Back.GREEN+ "I am Python Developer")
